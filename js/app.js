1b1ab2fcc1be8413bc2c3b1bb321ac2add57f985
Amar-8 <84033938+Amar-8@users.noreply.github.com>
2024-01-25 09:32:32 +0100

    Update app.js
    adding service Worker

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../serviceWorker.js')
+    .then(function(registration) {
+      // Registrierung erfolgreich
+      console.log('Service Worker Registrierung erfolgreich');
+    }).catch(function(error) {
+      // Registrierung fehlgeschlagen
+      console.log('Service Worker Registrierung fehlgeschlagen: ', error);
+    });
+}
 
 // This function initializes user media
 function getUserMedia(options, successCallback, failureCallback) {
