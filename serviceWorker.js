02eddf9cf887e03338e80a512deda28078f29dd8
Amar-8 <84033938+Amar-8@users.noreply.github.com>
2024-01-25 16:08:07 +0100

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,15 +1,15 @@
-const myPWA = "my-pwa-aufgaben";
+const myPWA = "my-pwa-aufgaben-v1";
 const assets = [
   "index.html",
   "css/style.css"
 ];
 
 self.addEventListener("install", installEvent => {
-   console.log('Service Worker installing.');
+   console.log('Service Worker installing');
 });
 
 self.addEventListener('activate', event => {
-  console.log('Service Worker installing.');
+  console.log('Service Worker installing');
 });
 
 self.addEventListener("fetch", fetchEvent => {
